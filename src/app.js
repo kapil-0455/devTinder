03858dc1abe927a262633547364461aa477553bd
kapil-0455 <kapil0455cu@gmail.com>
2026-04-28 12:44:34 +0530

    Validations On User

--- src/app.js
+++ src/app.js
@@ -3,23 +3,73 @@ const connectDB = require('./config/database');
 const User = require('./models/user');
 const app = express();
 
-app.post('/signUp' , async(req , res)=>{
-    const user = new User({
-        firstName: 'Kapil',
-        lastName: 'Singh',
-        email: 'kapil0455cu@gmail.com',
-        password: '730041',
-        age: 22,
-        gender: 'Male'
+app.use(express.json());
 
-    });
+app.get('/user' , async(req , res)=>{
+    const userId = req.body._id;
+
+    try {
+        const user = await User.findById({_id : userId});
+        res.send(user);
+    } catch (error) {
+        res.status(400).send("Something Went wrong")
+    }
+})
+
+app.get('/feed' , async(req , res)=>{
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+app.delete('/user' , async(req , res)=>{
+    const userId = req.body.id;
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        res.send('User deleted succesfully')
+    } catch (error) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+app.patch('/user/:userId' , async(req , res)=>{
+    const userId = req.params?.userId;
+    const data = req.body;
+
+    try {
+        const ALLOWED_Updates = ["photoUrl" , "skills" , "gender" , "age" , "description"];
+        const isUpdateAllowed = Object.keys(data).every((k)=> ALLOWED_Updates.includes(k));
+
+        if(!isUpdateAllowed){
+            throw new Error("Update not Allowed");
+        }
+
+        if(data?.skills.length > 10){
+            throw new Error("Skills not greater than 10")
+        }
+        const user = await User.findByIdAndUpdate(userId , data , {runValidators : true});
+        console.log(user);
+        res.send('User updated succesfully');
+    } catch (error) {
+        res.status(400).send("Something went wrong: " + error.message);
+    }
+})
+
+
+app.post('/signUp' , async(req , res)=>{
+    const user = new User(req.body);
     try {
         await user.save();
         res.send("Data saved ")
     } catch (error) {
-        res.status(400).send('Not saved');
+        res.status(400).send('Something went wrong' + error.message);
     }
 })
+
+
 
 connectDB().
 then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,62 @@
 const mongoose = require('mongoose');
-
+const validator = require('validator')
 const userSchema = new mongoose.Schema({
     firstName:{
         type: String,
+        required : true,
+        min : 2,
+        max:50
     },
     lastName :{
         type : String 
     },
     email :{
-        type: String
+        type: String,
+        required:true,
+        lowercase:true,
+        trim : true,
+        unique:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is not Correct")
+            }
+        }
+
     },
     password :{
-        type: String
+        type: String,
+        required : true,
+
     },
     age:{
-        type: Number
+        type: Number,
+        min : 18
     },
     gender :{
-        type: String
+        type: String,
+        validate(value){
+            if(!["male" , "female" , "others"].includes(value)){
+                throw new Error("Gender data is not Defined")
+            }
+        }
+    },
+    photoUrl:{
+        type : String,
+        default : "https://imgs.search.brave.com/hrPEOI6ZTeAdutYXBBsEiam5Os8jkLZwAQ-quU0b-ps/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNDUv/OTQ0LzE5OS9zbWFs/bC9tYWxlLWRlZmF1/bHQtcGxhY2Vob2xk/ZXItYXZhdGFyLXBy/b2ZpbGUtZ3JheS1w/aWN0dXJlLWlzb2xh/dGVkLW9uLWJhY2tn/cm91bmQtbWFuLXNp/bGhvdWV0dGUtcGlj/dHVyZS1mb3ItdXNl/ci1wcm9maWxlLWlu/LXNvY2lhbC1tZWRp/YS1mb3J1bS1jaGF0/LWdyZXlzY2FsZS1p/bGx1c3RyYXRpb24t/dmVjdG9yLmpwZw",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Email is not Correct")
+            }
+        }
+    },
+    description :{
+        type : String,
+        default : "This is Default behaviour"
+    },
+    skills:{
+        type : [String]
     }
 
-})
+}, {timestamps : true})
 
 module.exports = mongoose.model("User" , userSchema);
