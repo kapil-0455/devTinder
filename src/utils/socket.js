01fab8a8a993796d7359753ef1d90d2d479741be
kapil-0455 <kapil0455cu@gmail.com>
2026-05-18 13:19:43 +0530

    Build connections recive chat send back chat

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,10 @@
 const socket = require('socket.io')
+const crypto = require('crypto')
 
+//securing your room Id
+const getSecretRoomId =({userId , targetUserId})=>{
+   return crypto.createHash('sha256').update([userId , targetUserId].sort().join("-")).digest('hex')
+}
 const intializeSocket = (server)=>{
     
     const io = socket(server , {
@@ -11,12 +16,28 @@ const intializeSocket = (server)=>{
 
     io.on('connection' , (socket)=>{
 
-        socket.on('joinChat' , ()=>{});
+        socket.on('joinChat' , ({firstName, userId , targetUserId})=>{
+            // we need to made room with unique Id for each conversation 
+            const roomId = getSecretRoomId({userId , targetUserId});
 
-        socket.on('sendMessage' , ()=>{});
+            console.log(firstName + " "+ "Joining room : " , roomId);
+            socket.join(roomId);
+        });
+
+        //whatever we send over there we recive that here
+        socket.on('sendMessage' , ({firstName , userId , targetUserId ,text})=>{
+            const roomId = getSecretRoomId({userId , targetUserId});
+            // whatever message we got from frontend -> we have to emit it to other user
+            
+            io.to(roomId).emit('messageRecived' , {
+                firstName ,
+                text,
+            })
+            
+            
+        });
 
         socket.on('disconnect' , ()=>{
-            console.log('User disconnected : ' + socket.id);
         })
     })
 
